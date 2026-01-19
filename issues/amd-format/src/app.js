2b4828c55a8a1ac3635370bb89dc7a09d7b0afb5
shizhong <shizhong@qunhemail.com>
2026-01-19 14:23:52 +0800

    add amd format issue

--- issues/amd-format/src/app.js
+++ issues/amd-format/src/app.js
@@ -6,6 +6,6 @@ window.define.amd = {};
 import(
   /* webpackChunkName: "test-amd-pkg" */
   'test-amd-pkg'
-).then((xx) => {
-  console.log('🚀 ~ app.js ~ xx:', xx);
+).then((mod) => {
+  console.log('🚀 ~ app.js ~ test-amd-pkg:', mod.foo);
 });
